55aeb74820f5392ac780bb2569cc6db6a47570d6
Alexandre Nanchen <alexandre.nanchen@idiap.ch>
2016-05-11 16:57:59 +0200

    [code] Update continuation count implementation.

--- code/training.R
+++ code/training.R
@@ -23,7 +23,7 @@ readSample <- function(fileName) {
     cat("Reading ", fileName,"\n")
     df <- tbl_df(read.table(fileName,allowEscapes = T, sep="|", stringsAsFactors = F)) %>%
         rename(text=V1)
-    df$text <- sapply(df$text, function(s) sprintf("<s> %s </s>",s))
+    df$text <- sapply(df$text, function(s) sprintf("%s",s))
     Encoding(df$text) <- "UTF-8"
     return(df)
 }
@@ -49,9 +49,8 @@ text2ngram <- function(sentences, ngramOrders=c(1)) {
         if (length(filteredIndices) > 0)
             ngrams <- ngrams[-filteredIndices,]
     
-        #Add id column
-        ngrams$id <- seq(1,nrow(ngrams))
-        ngrams <- dplyr::select(ngrams, id, token, freq=Freq)
+        #Re-order
+        ngrams <- dplyr::select(ngrams, token, freq=Freq)
         #Split token into words
         colNames <- sapply(seq(1,order), function(s) paste0("word",s))
         ngrams <- tidyr::separate(ngrams,"token",colNames," ")
@@ -70,10 +69,17 @@ text2ngram <- function(sentences, ngramOrders=c(1)) {
 # return a data table with the fixed columns and
 #        their marginalized counts
 #
-getMarginalizedCount <- function(dt, fixedIndices = c(1)) {
-    cat("Marginalizing on indices", fixedIndices, "\n")
+getMarginalizedCount <- function(dt, fixedIndices = c(1), cCount = F) {
+    cat("---- Marginalizing on indices", fixedIndices, "\n")
     print(head(dt, n=2))
     colNames <- names(dt)[fixedIndices]
+    index <- NULL
+    #We work on a copy of original table
+    if (cCount) {
+        cat("  ==> Marginalized counts\n")
+        dt$freq <- as.integer(dt$freq>0)
+    }
+    cat("-----------------------------\n")
     index <- dt[,lapply(.SD,sum), by=colNames,.SDcols="freq"]
     setkeyv(index, colNames)
     return (list(index=index, colNames=colNames))
@@ -90,11 +96,15 @@ getMarginalizedCount <- function(dt, fixedIndices = c(1)) {
 trainProb <- function(dt, discount=0.75) {
     wIndices <- grep("word*",names(dt))
     order <- length(wIndices)
+    cat("###### Normal counts, order", order, "\n")
     fixedIndices = wIndices[-order]
-    ret <- getMarginalizedCount(dt, fixedIndices)
+    ret <- getMarginalizedCount(dt, fixedIndices, F)
     setkeyv(dt, ret$colNames)
-    dt <- dt[ret$index]
-    dt$logprob <- log10(max(dt$freq-discount,0)/dt$i.freq)
+    dt <-  suppressWarnings(merge(dt, ret$index, all.x=T))
+    print(head(dt))
+    setnames(dt, "freq.x", "freq")
+    setnames(dt, "freq.y", "marginalizedCount")
+    dt$logprob <- log10(pmax(dt$freq-discount,0)/dt$marginalizedCount)
     return(dt)
 }
 
@@ -108,38 +118,53 @@ trainProb <- function(dt, discount=0.75) {
 #
 trainContinuationProb <- function(dtLower, dtHigher, discount=0.75) {
     wIndicesLower <- grep("word*",names(dtLower))
-    orderLower <- length(wIndicesLower)
     wIndicesHigher <- grep("word*",names(dtHigher))
-    orderHigher <- length(wIndicesHigher)
-    
-    #Continuation count
+    ######################
+    # Continuation count:
+    # 
+    # w1 w2     w1  w2 w3
+    # b  c  --> a   b  c
+    #       --> d   b  c
+    #          ---------
+    #           *   b  c
+    #
+    cat("###### Continuation count:\n")
     fixedIndices = wIndicesHigher[-1]
-    ret <- getMarginalizedCount(dtHigher, fixedIndices)
+    ret <- getMarginalizedCount(dtHigher, fixedIndices, T)
     
-    cat("Merging lower order column '", names(dtLower)[wIndicesLower], "' with index column '", 
-        ret$colNames, "' Index: \n", sep=""); print(ret$index[1,])
-    
+    cat("  --> Merging lower order column '", names(dtLower)[wIndicesLower], "' with index column '", 
+        ret$colNames, "'\nIndex: \n", sep=""); print(ret$index[1,])
     setkeyv(dtLower, names(dtLower)[wIndicesLower])
-    dtLower <- merge(dtLower, ret$index, by.x=names(dtLower)[wIndicesLower],
-                     by.y = ret$colNames, all.x=T)
+    dtLower <- suppressWarnings(merge(dtLower, ret$index, by.x=names(dtLower)[wIndicesLower],
+                     by.y = ret$colNames, all.x=T))
     dtLower[which(is.na(dtLower$freq.y))]$freq.y <- 0
-    setnames(dtLower,"freq.y","contCount")
+    setnames(dtLower, "freq.x", "freq")
+    setnames(dtLower, "freq.y", "contCount")
     
-    #Normalization count
+    ######################
+    # Normalization count:
+    # 
+    # w1 w2     w1  w2 w3
+    # b  c  --> a   b  c
+    #       --> d   b  c
+    #          ---------
+    #           *   b  *
+    #
+    cat("###### Normalization count:\n")
     fixedIndices = wIndicesHigher[-c(1, length(wIndicesHigher))]
-    ret <- getMarginalizedCount(dtHigher, fixedIndices)
+    ret <- getMarginalizedCount(dtHigher, fixedIndices, T)
     
-    cat("Merging lower order column '", names(dtLower)[1], "' with index column '", 
-        ret$colNames, "' Index: \n", sep="");  print(ret$index[1,])
-    
-    dtLower <- merge(dtLower, ret$index, by.x=names(dtLower)[1],
-                     by.y = ret$colNames, all.x=T)
-    
-    dtLower[which(is.na(dtLower$freq))]$freq <- 1
-    setnames(dtLower,"freq","totalMarginalized")
+    cat("  --> Merging lower order column '", names(dtLower)[1], "' with index column '", 
+        ret$colNames, "'\nIndex: \n", sep="");  print(ret$index[1,])
+    dtLower <- suppressWarnings(merge(dtLower, ret$index, by.x=names(dtLower)[1],
+                     by.y = ret$colNames, all.x=T))
+    dtLower[which(is.na(dtLower$freq.y))]$freq.y <- 1
+    setnames(dtLower,"freq.x","freq")
+    setnames(dtLower,"freq.y","totalMarginalized")
     
     #Probability computation
     dtLower$logprob <- log10(pmax(dtLower$contCount-discount,0)/dtLower$totalMarginalized)
+    dtLower[which(is.infinite(dtLower$logprob))]$logprob <- (-100)
     return(dtLower)
 }
 
@@ -180,7 +205,11 @@ gram3 <- trainProb(gram3)
 
 #2-grams discounted countinuation probabilities
 gram2 <- ngramList[[2]]
-t <- trainContinuationProb(gram2, gram3)
+gram2 <- trainContinuationProb(gram2, gram3)
+
+#1-grams discounted countinuation probabilities
+gram1 <- ngramList[[1]]
+t <- trainContinuationProb(gram1, gram2)
 
 rmobj(fileName)
 rmobj(srcFile)
