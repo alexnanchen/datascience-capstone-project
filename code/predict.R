ba026bc70248c157e99e6cb4e6c520b5efd07c09
Alexandre Nanchen <alexnanchen@vtxmail.ch>
2016-05-16 12:54:44 +0200

    [code] Initial version of predict.

---
+++ code/predict.R
@@ -0,0 +1,77 @@
+library(dplyr)
+library(data.table)
+library(testit)
+
+###################
+# Environment
+#
+source("code/config.R")
+source("code/constants.R")
+source("code/mod.R")
+
+READBUFFER = 5000
+
+###################
+# Implementation
+#
+#-----------------------------------------------------
+# Read a n-gram model from disk
+#-----------------------------------------------------
+# param fileName  : the n-gram text file
+# return a data table
+#
+readModel <- function(fileName) {
+    cat("Reading ", fileName,"\n")
+    
+    #File size in lines
+    cmd <- sprintf("wc -l %s", fileName)
+    nbLines <- as.integer(strsplit(system(cmd, intern = T),fileName))
+    
+    #How many buffers do we have to read
+    nbBuffers <- (nbLines %/% READBUFFER) + as.integer((nbLines %% READBUFFER) > 0)
+    
+    cat(nbLines, "lines to read into", nbBuffers, "buffers of", READBUFFER,"sizes\n")
+    
+    #Read all buffers
+    df <- NULL; skip <- 0
+    for (i in seq(1,nbBuffers)) {
+        df <- readBuffer(fileName, skip, df)
+        skip <- skip + READBUFFER
+    } 
+   
+    assert(nrow(df) == nbLines)
+    
+    #Column names renaming
+    if ("backoffWeight" %in% names(df))
+        df <- dplyr::select(df, ngram=V1, logprob=V2, backoffWeight=V3)
+    else
+        df <- dplyr::select(df, ngram=V1, logprob=V2)
+    
+    return(data.table(df))
+}
+
+#-----------------------------------------------------
+# Read a buffer of n-grams
+#-----------------------------------------------------
+# param fileName  : the n-gram text file
+#       skip      : number of rows to skip
+#       df        : where to append the rows buffer
+# return a data frame
+#
+readBuffer <- function(fileName, skip, df) {
+    #Read in memory buffer
+    dfBuffer <- tbl_df(read.table(fileName,allowEscapes = T, sep="|", 
+                        stringsAsFactors = F, nrow=READBUFFER, skip=skip))
+    if (is.null(df))
+        df <- dfBuffer
+    else
+        df <- bind_rows(df, dfBuffer)
+    return(df)
+}
+
+###################
+# Main
+#
+dt <- readModel("gram1.txt")
+dt <- readModel("gram2.txt")
+dt <- readModel("gram3.txt")
