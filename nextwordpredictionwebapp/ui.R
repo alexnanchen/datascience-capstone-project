c4fe8f87a75976d34531ea37a789cf1b30b84715
Alexandre Nanchen <alexnanchen@vtxmail.ch>
2016-05-28 17:31:35 +0200

    [webapp] Add algorithm description.

--- nextwordpredictionwebapp/ui.R
+++ nextwordpredictionwebapp/ui.R
@@ -39,6 +39,37 @@ shinyUI(
                   )
               )
           ),
-          tabPanel("Algorithm"),
-          tabPanel("Models")
+          tabPanel("Help"),
+          tabPanel("Algorithm",
+                   mainPanel(
+                       div(id="models", class="well",
+                           HTML("<h2>N-gram model</h2>
+                                 <p>The deployed model is a 4-grams Kneser-Ney backoff model with a vocabulary
+                                    of 41'376 words, including the '<i>&lt;unk&gt;</i>' symbol for unknown words.
+                                    Here is a details of the counts:</p>
+                                      <ul><li>1-gram: 41376 entries</li>
+                                          <li>2-gram: 164534 entries</li>
+                                          <li>3-gram: 331930 entries</li>
+                                          <li>4-gram: 183427 entries</li>
+                                      </ul>
+                                 <p class='doc'>In order to save memory, part of the text composing the n-gram is converted into
+                                    a signed integer. This allows for a gain of memory of a factor of 2.6.
+                                 </p>"),
+                           HTML("<h2>Algorithm</h2>
+                                 <p>The following steps are performed in order to select the best words matches:
+                                    <ol><li>Prepare inputed words
+                                         <ul><li>Lower case all words</li>
+                                             <li>Replace unknow words with the '&lt;unk&gt;' token</li>
+                                         </ul>
+                                        <li>Select start N-gram from inputed words</li>
+                                        <li>Predict using highest order then backoff to lower orders until unigrams are reached</li>
+                                        <li>Keep all predictions of all orders with associated scores</li>
+                                        <li>Average all available scores for each word</li>
+                                        <li>Normalize top 10 scores to sum to 100%</li>
+                                        <li>Display results by decreasing model order</li>
+                                    </ol>
+                                 </p>")
+                           )
+                       )
+                   )
 )))
